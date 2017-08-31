0fddada85d7f1df1b4bae2dfd5a19fa059c7af35
Rafael Correia <rafael@jupiter.com.br>
2017-08-30 23:57:12 -0300

    Implementação de função com retorno inteiro

--- index.php
+++ index.php
@@ -43,31 +43,33 @@ function retornaDistanciaEntrePontoEReta($a, $b, $c)
 
 }
 
-$LatLinha = "-22.904186342480042,-22.9056885413639,-22.90966138188791,-22.907902277807445";
-$xLatLinha = explode(",",$LatLinha);
-
-$LonLinha = "-43.186912536621094,-43.191633224487305,-43.189830780029304,-43.18476676940919";
-$xLonLinha = explode(",",$LonLinha);
-
-$LatLon = "-22.906775648598042,-43.18845748901368";
-$xLatLon = explode(",",$LatLon);
-
-// ponto
-$c['lat'] = $xLatLon[0];
-$c['lon'] = $xLatLon[1];
-
-$arrayDistancia = [];
-for($i = 1; $i < count($xLatLinha); $i++)
+function retornarMenorDistancia($LatLinha, $LonLinha, $LatLon)
 {
-	// reta
-	$a['lat'] = $xLatLinha[$i];
-	$a['lon'] = $xLonLinha[$i];
-	$b['lat'] = $xLatLinha[$i-1];
-	$b['lon'] = $xLonLinha[$i-1];
-	$arrayDistancia[] = retornaDistanciaEntrePontoEReta($a, $b, $c)*1000 . PHP_EOL;
+	$arrayLatLinha = explode(",",$LatLinha);
+	$arrayLonLinha = explode(",",$LonLinha);
+	$arrayLatLon = explode(",",$LatLon);
+
+	// ponto
+	$c['lat'] = $arrayLatLon[0];
+	$c['lon'] = $arrayLatLon[1];
+
+	$arrayDistancia = [];
+	for($i = 1; $i < count($arrayLatLinha); $i++)
+	{
+		// reta
+		$a['lat'] = $arrayLatLinha[$i];
+		$a['lon'] = $arrayLonLinha[$i];
+		$b['lat'] = $arrayLatLinha[$i-1];
+		$b['lon'] = $arrayLonLinha[$i-1];
+		$arrayDistancia[] = retornaDistanciaEntrePontoEReta($a, $b, $c)*1000 . PHP_EOL;
+	}
+
+	return intval(min($arrayDistancia));
+
 }
 
-echo min($arrayDistancia);
-	
+$LatLinha = "-22.904186342480042,-22.9056885413639,-22.90966138188791,-22.907902277807445";
+$LonLinha = "-43.186912536621094,-43.191633224487305,-43.189830780029304,-43.18476676940919";
+$LatLon = "-22.906775648598042,-43.18845748901368";	
 
-
+echo retornarMenorDistancia($LatLinha, $LonLinha, $LatLon);
